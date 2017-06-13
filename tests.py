bed0a891f4d0d2ae9212552c570f372ca31d38b1
Al Avery <averyab0@sewanee.edu>
2017-06-13 15:15:08 -0500

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+import address_selection
+
+class TestAddressSelection(unittest.TestCase):
+
+	def test_r(self):
+		self.assertEqual(1,2)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
