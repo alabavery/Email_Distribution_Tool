dfff5d57240b436efdff8ec095b59fa5702a1dab
Al Avery <averyab0@sewanee.edu>
2017-06-14 18:02:58 -0500

    Change on_go_function() to main(), cleanup

--- index.py
+++ index.py
@@ -1,18 +1,24 @@
-import address_selection, file_io, gmail
+import address_selection, file_io, gmail, tkinter_gui, config
+
+SEEN_EMAIL_PATH = ""
+ALL_ADDRESSES_PATH = ""
+EXCEL_FILE_PATH = ""
+
+gmail_client = gmail.get_gmail_client()
 
 new_email_data = [('a',0),('b',0),('c',0)]
 
-# def on_go_function(): # parameters will be constants like json_paths, oauth stuff, etc.
+def main():
+	gmail_client = get_gmail_client()
+	unread_email_data = gmail.get_unread_email_data(gmail_client)
 	
-# 	"""
-# 	new_email_data = gmail.get_unread_emails()
-# 	seen_email_data = file_io.read_json_seen_email(seen_email_path)
-# 	all_addresses = file_io.read_json_seen_email(all_addresses_path)
-# 	"""
-	
-# 	for email in new_email_data:
-# 		d = address_selection.get_and_update_data_for_email(all_addresses, seen_email_data, email[0], email[1])
-# 		# gmail.send_email(d, email_address)
+	seen_email_data = file_io.read_json_seen_email(seen_email_path)
+	all_addresses = file_io.read_json_all_addresses(all_addresses_path)
+
+	for email in new_email_data:
+		d = address_selection.get_and_update_data_for_email(all_addresses, seen_email_data, email[0], email[1])
+		gmail.send_email(d, email_address)
+
 
 
 all_addresses = {'both_fields': list(range(200)), 'one_field_only': list(range(200,300))}
@@ -26,12 +32,6 @@ print(d)
 # gui = tkinter_gui.StartMenu(root, on_go_function, on_go_parameters)
 # root.mainloop()
 # root.destroy() # optional; see description below
-"""
-unread_emails = get_unread_emails()   -> this will be list of tuples, (email_address, postcard_indicator)
-for email in unread_emails:
-	selected_addresses = get_and_update_data_for_email(all_addresses, seen_email_data, email[0], email[1])
-	send_addresses(selected_addresses, email[0])
-"""
 
 
 	
