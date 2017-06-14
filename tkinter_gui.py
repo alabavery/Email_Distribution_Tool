70d532fd272907eba3dbc6e7c878991e510a42da
Al Avery <averyab0@sewanee.edu>
2017-06-14 00:54:53 -0500

    Create tkinter_gui.py

---
+++ tkinter_gui.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+class StartMenu:
+
+    def __init__(self, master, on_go_function, on_go_args):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(
+            frame, text="QUIT", fg="red", command=frame.quit
+            )
+        self.button.pack(side=LEFT)
+
+        self.on_go_function = on_go_function
+        self.on_go_args = on_go_args
+        self.run_processes = Button(frame, text="Run Program", command=self.on_go_function(on_go_args))
+        self.run_processes.pack(side=LEFT)
+       
+
+root = Tk()
+
+
+def sample_fxn(sample_args):
+    print('arg1:', sample_args['arg1'])
+    print('arg2:', sample_args['arg2'])
+    print('arg3:', sample_args['arg3'])
+
+sample_args = {'arg1': 1, 'arg2': 2, 'arg3': 3}
+
+
+menu = StartMenu(root, sample_fxn, sample_args)
+root.mainloop()
+root.destroy() # optional; see description below
