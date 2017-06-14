228ad3557d230fc30d9848eeb9a5ad9d3b8770e7
Al Avery <averyab0@sewanee.edu>
2017-06-14 01:02:14 -0500

    Add comment outline for use of tkinter to index.py

--- index.py
+++ index.py
@@ -1,12 +1,31 @@
 import address_selection, file_io, gmail
 
+new_email_data = [('a',0),('b',0),('c',0)]
+
+# def on_go_function(): # parameters will be constants like json_paths, oauth stuff, etc.
+	
+# 	"""
+# 	new_email_data = gmail.get_unread_emails()
+# 	seen_email_data = file_io.read_json_seen_email(seen_email_path)
+# 	all_addresses = file_io.read_json_seen_email(all_addresses_path)
+# 	"""
+	
+# 	for email in new_email_data:
+# 		d = address_selection.get_and_update_data_for_email(all_addresses, seen_email_data, email[0], email[1])
+# 		# gmail.send_email(d, email_address)
 
 
 all_addresses = {'both_fields': list(range(200)), 'one_field_only': list(range(200,300))}
 seen_email_data = []
 postcard_indicator = 0
 email_address = 'a'
+d = address_selection.get_and_update_data_for_email(all_addresses, seen_email_data, email_address, postcard_indicator)
+print(d)
 
+# root = tkinter.Tk()
+# gui = tkinter_gui.StartMenu(root, on_go_function, on_go_parameters)
+# root.mainloop()
+# root.destroy() # optional; see description below
 """
 unread_emails = get_unread_emails()   -> this will be list of tuples, (email_address, postcard_indicator)
 for email in unread_emails:
