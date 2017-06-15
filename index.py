86b33c560f77d6421c2a868fc83d35f694789d2f
Al Avery <averyab0@sewanee.edu>
2017-06-15 16:28:20 -0500

    Add shebang and add EXCEL_FILE_PATH

--- index.py
+++ index.py
@@ -1,14 +1,9 @@
+#!CodeBlue/bin/python3.5
 import address_selection, file_io, gmail, tkinter_gui, config
 
-SEEN_EMAIL_PATH = ""
-ALL_ADDRESSES_PATH = ""
-EXCEL_FILE_PATH = ""
-
-
-all_addresses = {'both_fields': list(range(200)), 'one_field_only': list(range(200,300))}
-seen_email_data = []
-postcard_indicator = 0
-email_address = 'a'
+SEEN_EMAIL_PATH = "json/seen_email_data.json"
+ALL_ADDRESSES_PATH = "json/all_addresses.json"
+EXCEL_FILE_PATH = "demo_csv.csv"
 
 
 def main(excel_file_path, seen_email_file_path, all_addresses_file_path):
@@ -17,12 +12,14 @@ def main(excel_file_path, seen_email_file_path, all_addresses_file_path):
 	unread_email_data = gmail.get_unread_email_data(gmail_client)
 
 	for sender in unread_email_data:
-		d = address_selection.get_and_update_data_for_email(all_addresses, seen_email_data, sender)
-		print(d)
-	# 	#gmail.send_email(d, email_address, gmail_client)
+		d = address_selection.get_and_update_data_for_email(existing_data['all_addresses'], existing_data['seen_email_data'], sender)
+		#gmail.send_email(d, gmail_client)
+
+	file_io.write_json_file(existing_data['all_addresses'],all_addresses_file_path)
+	file_io.write_json_file(existing_data['seen_email_data'],seen_email_file_path)
 
 
-
+main(EXCEL_FILE_PATH, SEEN_EMAIL_PATH, ALL_ADDRESSES_PATH)
 # root = tkinter.Tk()
 # gui = tkinter_gui.StartMenu(root, on_go_function, on_go_parameters)
 # root.mainloop()
