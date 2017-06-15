d78a38c67667479bbb0d3757f94353d3ccc216cb
Al Avery <averyab0@sewanee.edu>
2017-06-15 10:38:12 -0500

    Add create_message() and send_message() to gmail.py

--- gmail.py
+++ gmail.py
@@ -78,6 +78,45 @@ def get_unread_email_data(gmail_client):
 		yield sender
 
 
+def create_message(sender, to, subject, message_text):
+	"""Create a message for an email.
+
+	Args:
+	sender: Email address of the sender.
+	to: Email address of the receiver.
+	subject: The subject of the email message.
+	message_text: The text of the email message.
+
+	Returns:
+	An object containing a base64url encoded email object.
+	"""
+	message = MIMEText(message_text)
+	message['to'] = to
+	message['from'] = sender
+	message['subject'] = subject
+	return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+def send_message(service, message):
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId='me', body=message)
+               .execute())
+    print('Message Id: %s' % message['id'])
+    return message
+  except errors.HttpError, error:
+    print('An error occurred: %s' % error)
+
+
 def send_email(recipient_email, email_subject, email_body, gmail_client):
 	pass
 
