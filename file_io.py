2b7029320c6547dc841f194d7229ef9440033b6f
Al Avery <averyab0@sewanee.edu>
2017-06-14 19:01:12 -0500

    Combine 2 fxns into file_io.read_json_file(), update file_io.get_existing_data()

--- file_io.py
+++ file_io.py
@@ -1,3 +1,4 @@
+import json
 
 def read_excel(excel_file_path):
 	"""
@@ -5,11 +6,20 @@ def read_excel(excel_file_path):
 	if so, read into and return variable
 	if not, return None
 	"""
+	pass
 
 
-def update_all_addresses_from_excel(excel_file_path, json_file_path):
-	uptodate_excel = read_excel(excel_file_path)
+def read_json_file(json_file_path):
+	read_json = open(json_file_path, 'r').read()
+	return json.loads(read_json)
+
+
+def get_existing_data(excel_file_path, seen_email_file_path, all_addresses_file_path):
+	updated_excel = read_excel(excel_file_path)
 	
 	if uptodate_excel:
-		"""write over json file"""
-		pass
+		return updated_excel['all_addresses'], uptodate_excel['seen_email_']
+	else:
+		seen_email_data = read_json_file(seen_email_file_path)
+		all_addresses = read_json_file(all_addresses_file_path)
+		return {'seen_email_data':seen_email_data, 'all_addresses':all_addresses}
