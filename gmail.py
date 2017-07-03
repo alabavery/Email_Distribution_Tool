0375324f2ca325d99dffd313ee6be65ddf1e8c13
Al Avery <averyab0@sewanee.edu>
2017-07-03 09:08:15 -0500

    Add old get_client, send, and receive logic to gmail.py

--- gmail.py
+++ gmail.py
@@ -47,3 +47,78 @@ def get_credentials():
             credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
+
+
+def get_gmail_client():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+    return service
+
+
+def get_unread_email_ids(gmail_client):
+    """
+    return list of id of unread emails
+    """
+    response = gmail_client.users().messages().list(userId='me',q='is:unread').execute()
+    ids = [message['id'] for message in response['messages']]
+    return ids
+
+
+def get_unread_email_data(gmail_client):
+    """
+    Use gmail api to find new emails
+    Return email addresses of new emails and whether or not they have postcard
+    """
+    unread_ids = get_unread_email_ids(gmail_client)
+
+    for message_id in unread_ids:
+        message_data = gmail_client.users().messages().get(userId='me',id=message_id).execute()
+        message_headers = message_data['payload']['headers']
+        sender = [header['value'] for header in message_headers if header['name'] == 'Return-Path'][0]
+        yield sender
+
+
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+
+    Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+    Returns:
+    An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    raw = base64.urlsafe_b64encode(message.as_bytes())
+    return {'raw':raw.decode()}
+
+
+def send_message(message, service):
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId='me', body=message).execute())
+    print('Message Id: %s' % message['id'])
+    return message
+  except errors.HttpError as error:
+    print('An error occurred: %s' % error)
+
+
+def send_email(host_email, recipient_email, email_subject, email_body, gmail_client):
+    message = create_message(host_email, recipient_email, email_subject, email_body)
+    send_message(message, gmail_client)
