c3fb3b0c88fb7426b4b987262ce61ea9fb2f8cc2
Al Avery <averyab0@sewanee.edu>
2017-07-03 08:43:39 -0500

    Commit before new branch

---
+++ config.py
@@ -0,0 +1,5 @@
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/gmail-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
