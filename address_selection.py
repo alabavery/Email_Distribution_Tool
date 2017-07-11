f28cbfb8fbb9b0ddf47ef9a9da7e1c7b80300938
Al Avery <averyab0@sewanee.edu>
2017-07-11 17:57:40 -0500

    update address_selection.get_and_update_data_for_email()

--- address_selection.py
+++ address_selection.py
@@ -7,17 +7,20 @@ def get_and_update_data_for_email(all_addresses, seen_email_data, email_address,
 	attachment: bool True if email has attachment
 	"""
 	this_emails_seen_index = [i for i,email_data in enumerate(seen_email_data) if email_data['email'] == email_address]
+	email_address_seen_before = len(this_emails_seen_index) == 1
 
-	if len(this_emails_seen_index) == 1:
-		if attachment:
+	if email_address_seen_before and not attachment:
+		print("Sender " + seen_email_data[this_emails_seen_index[0]] + " previously requested and has not included attachment.")
+		return False
+
+	else:
+		selection = all_addresses['both_fields'][-10:]
+		del all_addresses['both_fields'][-10:]
+
+		if email_address_seen_before:
 			seen_email_data[this_emails_seen_index[0]]['both_fields'].extend(selection)
 		else:
-			return False # Sender has tried to get 10 more addresses without sending postcard.  This is not allowed.
+			new_entry = {'email': email_address, 'one_field_only': [], 'both_fields': selection}
+			seen_email_data.append(new_entry)
 
-	else:		
-		new_entry = {'email': email_address, 'one_field_only': [], 'both_fields': selection}
-		seen_email_data.append(new_entry)
-
-	selection = all_addresses['both_fields'][-10:]
-	del all_addresses['both_fields'][-10:]
-	return selection
+		return selection
