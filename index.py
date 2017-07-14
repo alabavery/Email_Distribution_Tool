6942d611d678e029922aac339ee5eaf234552c17
Al Avery <averyab0@sewanee.edu>
2017-07-14 09:59:13 -0500

    Update index.py in-line with changes to file_management.py

--- index.py
+++ index.py
@@ -14,33 +14,34 @@ address_json_file_path = os.path.join(data_dir_path, config.ADDRESSES_JSON_FILE_
 client_secret_file_path = os.path.join(data_dir_path, config.CLIENT_SECRET_FILE_NAME)
 client_secret = config.CLIENT_SECRET
 
-# looks for data dir, ensures it contains csv, and creates dir and prompts user for csv if not found
-data_existed_already = file_management.ensure_data_exists(data_dir_path, config.SEEN_EMAIL_FILE_NAME, config.CLIENT_SECRET_FILE_NAME, config.CLIENT_SECRET)
-#if data_existed_already:
-#	all_addresses = file_io.get_json_data(address_json_file_path)
-#	used_addresses = all_addresses['used']
-#	unused_addresses = all_addresses['unused']
-#else:
-csv_file_path = file_management.get_csv_file_path(data_dir_path)
-all_addresses = file_io.get_csv_addresses(csv_file_path)
-used_addresses = []
-unused_addresses = all_addresses
+# looks for data dir, ensures it contains csv, creates dir and prompts user for csv if not found, then copies rest of necessary data
+file_management.ensure_data_exists(data_dir_path, 
+									config.ADDRESSES_JSON_FILE_NAME, 
+									config.SEEN_EMAIL_FILE_NAME, 
+									config.CLIENT_SECRET_FILE_NAME, 
+									config.CLIENT_SECRET)
 
-both_fields_addresses = all_addresses['both_fields']
-one_field_addresses = all_addresses['one_field_only']
-seen_email_data = file_io.get_seen_email_data(seen_email_file_path)
+# three lines below assume that csv file WILL NOT CHANGE after initial saving
+all_addresses = file_io.get_json_data(address_json_file_path)
+used_addresses = all_addresses['used']
+unused_addresses = all_addresses['unused']
+
+both_fields_addresses = unused_addresses['both_fields']
+one_field_addresses = unused_addresses['one_field_only']
+seen_email_data = file_io.get_json_data(seen_email_file_path)
+
 
 gmail_client = gmail.get_gmail_client(data_dir_path, client_secret_file_path, config.SCOPES, config.APPLICATION_NAME)
 new_email_data = gmail.get_unread_email_data(gmail_client)
 
-for email in new_email_data:
-	addresses_to_send = address_selection.get_and_update_data_for_email(all_addresses, seen_email_data, email[0], email[1])
-	print(addresses_to_send)
-	print("#################################")
-	if addresses_to_send:
-		gmail.send_addresses(config.APPLICATION_EMAIL, email[0], config.SUBJECT, addresses_to_send, config.INTRO_TEXT, gmail_client)
-	else:
-		gmail.send_rejection(config.APPLICATION_EMAIL, email[0], config.REJECTION_SUBJECT, config.REJECTION_TEXT, gmail_client)
+# for email in new_email_data:
+# 	addresses_to_send = address_selection.get_and_update_data_for_email(all_addresses, seen_email_data, email[0], email[1])
+# 	print(addresses_to_send)
+# 	print("#################################")
+# 	if addresses_to_send:
+# 		gmail.send_addresses(config.APPLICATION_EMAIL, email[0], config.SUBJECT, addresses_to_send, config.INTRO_TEXT, gmail_client)
+# 	else:
+# 		gmail.send_rejection(config.APPLICATION_EMAIL, email[0], config.REJECTION_SUBJECT, config.REJECTION_TEXT, gmail_client)
 
 # file_io.write_seen_email_data(seen_email_file_path, seen_email_data)
 
