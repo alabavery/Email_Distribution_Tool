474800e96ab380390eec3a483e51230826d84f4a
Al Avery <averyab0@sewanee.edu>
2017-07-14 15:03:34 -0500

    Changes to index.py

--- index.py
+++ index.py
@@ -16,33 +16,34 @@ client_secret = config.CLIENT_SECRET
 
 # looks for data dir, ensures it contains csv, creates dir and prompts user for csv if not found, then copies rest of necessary data
 file_management.ensure_data_exists(data_dir_path, 
-									config.ADDRESSES_JSON_FILE_NAME, 
-									config.SEEN_EMAIL_FILE_NAME, 
-									config.CLIENT_SECRET_FILE_NAME, 
-									config.CLIENT_SECRET)
+									address_json_file_path, 
+									seen_email_file_path, 
+									client_secret_file_path, 
+									client_secret)
 
-# three lines below assume that csv file WILL NOT CHANGE after initial saving
+# WARNING:::: three lines below assume that csv file WILL NOT CHANGE after initial saving
 all_addresses = file_io.get_json_data(address_json_file_path)
 used_addresses = all_addresses['used']
 unused_addresses = all_addresses['unused']
 
-both_fields_addresses = unused_addresses['both_fields']
-one_field_addresses = unused_addresses['one_field_only']
 seen_email_data = file_io.get_json_data(seen_email_file_path)
-
-
 gmail_client = gmail.get_gmail_client(data_dir_path, client_secret_file_path, config.SCOPES, config.APPLICATION_NAME)
 new_email_data = gmail.get_unread_email_data(gmail_client)
 
-# for email in new_email_data:
-# 	addresses_to_send = address_selection.get_and_update_data_for_email(all_addresses, seen_email_data, email[0], email[1])
-# 	print(addresses_to_send)
-# 	print("#################################")
-# 	if addresses_to_send:
-# 		gmail.send_addresses(config.APPLICATION_EMAIL, email[0], config.SUBJECT, addresses_to_send, config.INTRO_TEXT, gmail_client)
-# 	else:
-# 		gmail.send_rejection(config.APPLICATION_EMAIL, email[0], config.REJECTION_SUBJECT, config.REJECTION_TEXT, gmail_client)
+for email in new_email_data:
+	addresses_to_send = address_selection.get_and_update_data_for_email(unused_addresses, 
+																		used_addresses, 
+																		seen_email_data, 
+																		email[0], # sender address
+																		email[1]) # attachment bool var
 
-# file_io.write_seen_email_data(seen_email_file_path, seen_email_data)
+	if addresses_to_send: # will be false if sender who has been seen before but did not send attachment
+ 		gmail.send_addresses(config.APPLICATION_EMAIL, email[0], config.SUBJECT, addresses_to_send, config.INTRO_TEXT, gmail_client)
+	else:
+		gmail.send_rejection(config.APPLICATION_EMAIL, email[0], config.REJECTION_SUBJECT, config.REJECTION_TEXT, gmail_client)
+
+new_all_addresses = {'used': used_addresses, 'unused': unused_addresses}
+file_io.write_json_data(address_json_file_path, new_all_addresses)
+file_io.write_json_data(seen_email_file_path, seen_email_data)
 
 
